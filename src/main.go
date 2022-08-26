6012cbe1426eb47fe0cc67aff1df2852293d8a62
miguel <meqh1992@gmail.com>
2022-08-26 17:26:32 -0500

    add base cuadrado

--- src/main.go
+++ src/main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola mundo")
+	// declaracion de constantes
+	const p1 float64 = 3.14
+	const p2 float32 = 3.14
+	fmt.Println("pi :", p1)
+	fmt.Println("p2 :", p2)
+	// Declaracion de variables
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println("base", base)
+	fmt.Println("altura", altura)
+	fmt.Println("area", area)
+
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	const baseCuadrado = 10
+	areacuadradao := baseCuadrado * baseCuadrado
+	fmt.Println(areacuadradao)
+
 }
