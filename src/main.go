622233a23d7ebf1860cd4873965e1388c1a36092
miguel <meqh1992@gmail.com>
2022-08-30 21:48:27 -0500

    recorrer

--- src/main.go
+++ src/main.go
@@ -2,28 +2,33 @@ package main
 
 import "fmt"
 
+func isPalindromo(text string) {
+	var textCopy string
+	for i := len(text) - 1; i >= 0; i-- {
+		textCopy += string(text[i])
+	}
+	if text == textCopy {
+		fmt.Println("is paili")
+	} else {
+		fmt.Println("no es pali")
+	}
+}
 func main() {
-	//array
-	var array [4]int
-	array[0] = 1
-	fmt.Println(array, len(array), cap(array))
 
-	// slice
-	slice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	fmt.Println(slice, len(slice), cap(slice))
+	slice := []string{"Hola", "Mundo"}
 
-	fmt.Println(slice[0])
-	fmt.Println(slice[:3])
-	fmt.Println(slice[2:4])
-	fmt.Println(slice[4:])
+	for i, value := range slice {
+		fmt.Println(i, value)
+	}
 
-	// add
-	slice = append(slice, 7)
+	for _, value := range slice {
+		fmt.Println(value)
+	}
 
-	//add list
-	newSlice := []int{11, 12}
-	slice = append(slice, newSlice...)
+	for i := range slice {
+		fmt.Println(i)
+	}
 
-	fmt.Println(slice)
-
+	isPalindromo("hola")
+	isPalindromo("ama")
 }
