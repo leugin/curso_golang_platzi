e8019f8368ce4b6fee2ff29d9c779cc57d592cb8
miguel <meqh1992@gmail.com>
2022-08-31 16:21:32 -0500

    add go rutines 1

--- src/main.go
+++ src/main.go
@@ -1,44 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
-type figurad2D interface {
-	area() float64
-}
-type cuadrado struct {
-	base float64
-}
+func say(text string, wg *sync.WaitGroup) {
 
-type rectangulo struct {
-	base   float64
-	altura float64
-}
-
-func calcular(fig figurad2D) {
-	fmt.Println(fig.area())
-}
-
-func (c cuadrado) area() float64 {
-	return c.base * c.base
-}
-
-func (c rectangulo) area() float64 {
-	return c.base * c.altura
+	defer wg.Done()
+	fmt.Println(text)
 }
 func main() {
-	myCuadrado := cuadrado{base: 5}
-	myRectanguls := rectangulo{base: 5, altura: 2}
+	var wg sync.WaitGroup
+	fmt.Println("Hola")
+	go say("Mundo", &wg)
+	wg.Add(1)
+	wg.Wait()
+	// funcones anonimas
 
-	fmt.Println(myCuadrado.area())
-	fmt.Println(myRectanguls.area())
-
-	// por interfaz
-	calcular(myCuadrado)
-	calcular(myRectanguls)
-
-	/// lita de interfaces
-
-	myInterfaes := []interface{}{"Hola", 5, 20}
-
-	fmt.Println(myInterfaes...)
+	go func(text string) {
+		fmt.Println(text)
+	}("Adios")
+	time.Sleep(time.Second * 1)
 }
