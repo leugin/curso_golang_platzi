df048fcafd9952e908e875c3366a401a873c9f07
miguel <meqh1992@gmail.com>
2022-08-31 15:49:19 -0500

    overload string

--- src/main.go
+++ src/main.go
@@ -8,31 +8,12 @@ type pc struct {
 	disk  int
 }
 
-func (myPc pc) ping() {
-
-	fmt.Println("brand ", myPc.brand, myPc.ram)
-}
-
-func (myPc *pc) dobleRam() {
-
-	myPc.ram = myPc.ram * 2
+func (myPc pc) String() string {
+	return fmt.Sprintf("tengo %d gb", myPc.ram)
 }
 
 func main() {
-	a := 50
-	b := &a // &se accede a la direccion de memoria
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(*b) // se accede al valor de la direccion
+	myPc := pc{brand: "lenovo", disk: 52, ram: 50}
 
-	*b = 110 // si modificamos *b
-
-	fmt.Println(a) // se modifica en el original tambien
-
-	myPc := pc{brand: "adf", disk: 5, ram: 50}
-	myPc.ping()
-
-	myPc.dobleRam()
-
-	myPc.ping()
+	fmt.Println(myPc)
 }
