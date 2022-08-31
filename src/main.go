d2d937434d4d88ba4da41400568ebe9e7a285645
miguel <meqh1992@gmail.com>
2022-08-31 17:25:48 -0500

    add go rutines 2

--- src/main.go
+++ src/main.go
@@ -2,25 +2,37 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
 )
 
-func say(text string, wg *sync.WaitGroup) {
-
-	defer wg.Done()
-	fmt.Println(text)
+func message(text string, c chan string) {
+	c <- text
 }
 func main() {
-	var wg sync.WaitGroup
-	fmt.Println("Hola")
-	go say("Mundo", &wg)
-	wg.Add(1)
-	wg.Wait()
-	// funcones anonimas
+	c := make(chan string, 2)
+	c <- "Mensaje"
+	c <- "mensaje 2"
 
-	go func(text string) {
-		fmt.Println(text)
-	}("Adios")
-	time.Sleep(time.Second * 1)
+	fmt.Println(len(c), cap(c))
+
+	close(c)
+
+	for message := range c {
+		fmt.Println(message)
+	}
+
+	/// select
+	email1 := make(chan string)
+	email2 := make(chan string)
+
+	go message("Hola", email1)
+	go message("Adios", email2)
+	for i := 0; i < 2; i++ {
+		select {
+		case n1 := <-email1:
+			fmt.Println("Respondio 1: ", n1)
+		case n2 := <-email2:
+			fmt.Println("respondio 2", n2)
+		}
+	}
+
 }
