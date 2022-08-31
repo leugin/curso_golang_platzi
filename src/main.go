80768c7863cff9661d4780c3742cb3dace0b683b
miguel <meqh1992@gmail.com>
2022-08-30 21:01:11 -0500

    funciones

--- src/main.go
+++ src/main.go
@@ -2,21 +2,33 @@ package main
 
 import "fmt"
 
-func main() {
-	hello := "Hola"
-	fmt.Println(hello)
-	nombre := "platzi"
-	cursos := 500
-
-	//printf
-	fmt.Printf("%s hola tiene mas de %d\n", nombre, cursos)
-	fmt.Printf("%v hola tiene mas de %v\n", nombre, cursos)
-
-	//Spring
-	message := fmt.Sprintf("%s hola tiene mas de %d\n", nombre, cursos)
-
+func normalFunction(message string) {
 	fmt.Println(message)
+}
 
-	// tipo de datos
-	fmt.Printf("Hello %T\n", message)
+func tripeAdmin(a, b int, c string) {
+	fmt.Println(a, b, c)
+}
+
+func returnValue(a int) int {
+	return a + 2
+}
+
+func returnValue2(a int) (c, d int) {
+	return a, a * 2
+}
+
+func main() {
+	normalFunction("Hola mundo")
+	tripeAdmin(1, 2, "3")
+
+	value := returnValue(5)
+	fmt.Println(value)
+
+	value1, value2 := returnValue2(2)
+	fmt.Println(value1, value2)
+
+	// tomando solo e primer val
+	value3, _ := returnValue2(2)
+	fmt.Println(value3)
 }
