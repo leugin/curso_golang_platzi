93082fba112fead1a12566fa233dc7a1aca92e07
miguel <meqh1992@gmail.com>
2022-08-31 15:44:36 -0500

    punteros

--- src/main.go
+++ src/main.go
@@ -1,14 +1,38 @@
 package main
 
-import (
-	"curso_golang_platzi/src/mypackage"
-	"fmt"
-)
+import "fmt"
+
+type pc struct {
+	brand string
+	ram   int
+	disk  int
+}
+
+func (myPc pc) ping() {
+
+	fmt.Println("brand ", myPc.brand, myPc.ram)
+}
+
+func (myPc *pc) dobleRam() {
+
+	myPc.ram = myPc.ram * 2
+}
 
 func main() {
-	var myCar mypackage.CarPublic
-	myCar.Brand = "Hola"
-	fmt.Println(myCar)
+	a := 50
+	b := &a // &se accede a la direccion de memoria
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(*b) // se accede al valor de la direccion
 
-	mypackage.PrintMensaje()
+	*b = 110 // si modificamos *b
+
+	fmt.Println(a) // se modifica en el original tambien
+
+	myPc := pc{brand: "adf", disk: 5, ram: 50}
+	myPc.ping()
+
+	myPc.dobleRam()
+
+	myPc.ping()
 }
