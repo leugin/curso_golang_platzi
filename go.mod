85ca9a9ed181dfad753e13c4aa8c45a3c6fa0d67
miguel <meqh1992@gmail.com>
2022-09-07 14:24:58 -0500

    add echo server

--- go.mod
+++ go.mod
@@ -1,5 +1,17 @@
-module curso_golang_platzi
+module github.com/leugin/curso_golang_platzi
 
 go 1.19
 
-require golang.org/x/tour v0.1.0 // indirect
+require (
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
+	golang.org/x/net v0.0.0-20220907135653-1e95f45603a7 // indirect
+	golang.org/x/sys v0.0.0-20220907062415-87db552b00fd // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tour v0.1.0 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -1,38 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
 )
 
-func message(text string, c chan string) {
-	c <- text
-}
 func main() {
-	c := make(chan string, 2)
-	c <- "Mensaje"
-	c <- "mensaje 2"
-
-	fmt.Println(len(c), cap(c))
-
-	close(c)
-
-	for message := range c {
-		fmt.Println(message)
-	}
-
-	/// select
-	email1 := make(chan string)
-	email2 := make(chan string)
-
-	go message("Hola", email1)
-	go message("Adios", email2)
-	for i := 0; i < 2; i++ {
-		select {
-		case n1 := <-email1:
-			fmt.Println("Respondio 1: ", n1)
-		case n2 := <-email2:
-			fmt.Println("respondio 2", n2)
-		}
-	}
-
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
